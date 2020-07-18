f61a2579939bc822dde56138648b4373b9e2985a
Sreenikhil <kollu1996@gmail.com>
2020-07-18 18:24:13 -0400

    Container with most water

--- src/com/company/Problem2.java
+++ src/com/company/Problem2.java
@@ -1,7 +1,33 @@
 package com.company;
-
+import java.util.*;
+// Container with maximum water O(n*n)
 public class Problem2 {
     public static void main(String []args){
-        System.out.println("I am a problem 2");
+
+        // Container with maximum water O(N*N)
+        int n;
+        int result = 0;
+        System.out.println("Enter length of the array");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int p=0; p<n; p++) arr[p] = sc.nextInt();
+        /*
+        for(int j=0; j<n; j++){
+            for(int k=n-1; k>=0; k--){
+                 result = Math.max(result, Math.abs(k-j) * Math.min(arr[j],arr[k]));
+            }
+        }
+        System.out.println("Maximum Area is: "+ result);*/
+
+        // O(N) solution, 2 pointer technique
+        int p=0;
+        int q=n-1;
+        while(p < n && q >= 0){
+            result = Math.max(result, Math.abs(q-p) * Math.min(arr[p],arr[q]));
+            if(arr[p] < arr[q]) p++;
+            else q--;
+        }
+        System.out.println("Maximum Area is: "+ result);
     }
 }
